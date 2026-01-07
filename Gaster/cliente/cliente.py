723da791f9397d9ba8083ff4acc283bd71e47bd2
Raquel <raquelmartiniano14@gmail.com>
2026-01-07 22:45:38 +0000

    fazendo a base cliente

--- Gaster/cliente/cliente.py
+++ Gaster/cliente/cliente.py
@@ -1,5 +1,9 @@
+import socker
+
 class Cliente:
-    def __init__(self):
+    def __init__(self, id_cliente, porta):
+        self.id_cliente = id_cliente
+        self.socker = socker.socker(socker.AF_INET, socket.SOCK_STREAM)
+        self.porta_servidor = porta
 
-
-        
+    def 
