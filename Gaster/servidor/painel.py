a7d6e11f49724e6c0bb3cf1cc1742424fa767401
Raquel <raquelmartiniano14@gmail.com>
2026-01-08 01:54:05 +0000

    colocando base do painel

--- Gaster/servidor/painel.py
+++ Gaster/servidor/painel.py
@@ -1,2 +1,9 @@
 class Painel:
-    def __init__(self):
+    def mostrar_cliente(self, id_cliente, armazenamento):
+        for id_cliente, cliente in armazenamento: #tá incompleto
+            print(f"ID: {id_cliente}")
+            print(f"Memória: {id_cliente}") #precisa chamar os outros depois, só é a base da ideia
+            print(f"CPU: {id_cliente}")
+            print(f"Disco: {id_cliente}")
+            print(f"Rede: {id_cliente}")
+            print(f"Sistema: {id_cliente}")
