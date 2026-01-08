47ceeef921de452ecc9e39a4eefcb6a379d2b3d2
Raquel <raquelmartiniano14@gmail.com>
2026-01-08 00:19:36 +0000

    segurança sendo feita

--- Gaster/cliente/segurança.py
+++ Gaster/cliente/segurança.py
@@ -1,2 +1,16 @@
+import secrets
+import time
+
 class Seguranca:
     def __init__(self):
+        self.tokens = {} #dicionario
+
+    def criar_token(self, id_cliente):
+        token = secrets.token_hex(16)
+
+        self.tokens[id_cliente] = { //para cada cliente, terá seu token e tempo criado
+            "token": token,
+            "criado_em": time.time()
+        }
+
+        return token
