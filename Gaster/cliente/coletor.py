03a0f251958daffd10184a422979d6a55708c7cd
Raquel <raquelmartiniano14@gmail.com>
2026-01-08 00:58:06 +0000

    fazendo do coletor aos poucos

--- Gaster/cliente/coletor.py
+++ Gaster/cliente/coletor.py
@@ -1,2 +1,48 @@
+import os
+import platform
+import socket
+
 class Coletor:
-    def __init__(self):
+    def pegou_cpu(self):
+        try:
+            return os.cpu_count()
+        except: 
+            return None
+
+    def pegou_disco(self):
+        try:
+            if platform.system() == "Windows":
+
+            else: #caso for Linux
+
+        except: #não achar nenhum
+            return None
+        
+    def pegou_memoria(self):
+        try:
+
+        except: 
+            return None
+
+    def pegou_sistema(self):
+        try:
+
+        except: 
+            return "Desconhecido, não foi encontrado"
+                
+
+    def pegou_rede(self):
+        try:
+
+        except: 
+
+
+    def coletou_tudo(self):
+        return {
+            "CPU": self.pegou_cpu(),
+            "Disco": self.pegou_disco(),
+            "Memória": self.pegou_memoria(),
+            "Rede": self.pegou_rede(),
+            "Sistema": self.pegou_sistema()
+        }
+        
