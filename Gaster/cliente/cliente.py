30b7d9dbd602d30d97510433cab1a42b3ff37a22
Raquel <raquelmartiniano14@gmail.com>
2026-01-12 05:58:20 +0000

    atualizei com coletor no cliente

--- Gaster/cliente/cliente.py
+++ Gaster/cliente/cliente.py
@@ -1,6 +1,7 @@
 import socket
 import json
 import time
+from coletor import Coletor
 
 class Cliente:
     def __init__(self, id_cliente, porta, ip):
@@ -8,6 +9,7 @@ class Cliente:
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.porta_servidor = porta
         self.ip_servidor = ip
+        self.coletor = Coletor()
 
     def conecta(self):
         self.socket.connect((self.ip_servidor, self.porta_servidor)) #endereço, é obrigatório ter (()) para dizer que é dupla
@@ -16,7 +18,9 @@ class Cliente:
         mensagem = {
             "Tipo" : "HELLO",
             "id_do_cliente" : self.id_cliente,
+            "Informações" : self.coletor.coletou_tudo(),
             "Tempo" : time.time()
+
         }
 
         dados = json.dumps(mensagem).encode() #transformar o texto do dicionário em bytes - dados
