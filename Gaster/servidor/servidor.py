84088b43b6c5766ddb6200ec581b570be3a3fdae
Raquel <raquelmartiniano14@gmail.com>
2026-01-12 06:10:55 +0000

    fazendo do servidor, mas precisa mudar

--- Gaster/servidor/servidor.py
+++ Gaster/servidor/servidor.py
@@ -1,3 +1,23 @@
-class Servidor:
-    def __init__(self):
-        
+import socket
+import json
+
+class Servidor: #não está totalmente completo para responder varios clientes
+    def __init__(self, porta, ip):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.porta_servidor = porta
+        self.ip_servidor = ip
+
+    def ouvir(self):
+        self.socket.bind((self.ip_servidor, self.porta_servidor))
+        self.socket.listen()
+        print("Ouvindo. . .")
+
+    def responder(self):
+        conexao, endereco = self.socket.accept()
+        print(f"Peguei o cliente: {endereco}")
+
+        dados = conexao.recv(4096) #quantidade de letras para receber 
+        mensagem = json.loads(dados.decode()) #novamente converte byte em texto
+
+        print("Mensagem recebida:")
+        print(mensagem)
